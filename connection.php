32a2404a038f2d708e2ae94d3bedc4cdd186e75c
AlexFlash27 <68725809+AlexFlash27@users.noreply.github.com>
2022-03-16 12:24:25 +0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$username = "root";
-$pass = "root";
-$host = "192.168.7.1";
+$username = "";
+$pass = "";
+$host = "";
 $db_name = "yii2test";
 $con = mysqli_connect ($host, $username, $pass);
 $db = mysqli_select_db ( $con, $db_name );
